234612750b5bc4a0f403ab0db9d2807262386e95
Amanda Tjan <amanda.tjan@gmail.com>
2017-12-06 21:01:15 -0700

    Finish centipede-whole test and fix some spelling errors

--- test/centipede-seg-test.js
+++ test/centipede-seg-test.js
@@ -13,33 +13,33 @@ describe('CentipedeSeg', function() {
     centipedeSeg = new CentipedeSeg(120, 120, 6, 24, 12, 'blue');
   });
 
-  it('expect to return true', function() {
+  it('expects to return true', function() {
     expect(true).to.equal(true);
   });
 
-  it('expect to not have reached lower half by default', function() {
+  it('expects to not have reached lower half by default', function() {
     expect(centipedeSeg.reachedLowerHalf).to.equal(false);
   }); 
 
-  it('expect to have an x and y', function() {
+  it('expects to have an x and y', function() {
     expect(centipedeSeg.x).to.equal(120);
     expect(centipedeSeg.y).to.equal(120);
   })
 
-  it('expect to have an x and y velocity', function() {
+  it('expects to have an x and y velocity', function() {
     expect(centipedeSeg.dx).to.equal(6);
     expect(centipedeSeg.dy).to.equal(24);
   });
 
-  it('expect to have a radius', function() {
+  it('expects to have a radius', function() {
     expect(centipedeSeg.radius).to.equal(12);
   });
 
-  it('expect to have a color', function() {
+  it('expects to have a color', function() {
     expect(centipedeSeg.color).to.equal('blue');
   })
 
-  it('expect to be able to move along the x axis', function() {
+  it('expects to be able to move along the x axis', function() {
     expect(centipedeSeg.x).to.equal(120);
     
     centipedeSeg.move();
@@ -47,7 +47,7 @@ describe('CentipedeSeg', function() {
     expect(centipedeSeg.x).to.equal(126);
   });
 
-  it('expect to switch x direction when segment hits the left canvas wall', function() {
+  it('expects to switch x direction when segment hits the left canvas wall', function() {
     let centipedeSeg = new CentipedeSeg(12, 36, -6, 24, 12, 'blue');
 
     centipedeSeg.move();
@@ -59,7 +59,7 @@ describe('CentipedeSeg', function() {
     expect(centipedeSeg.dx).to.equal(6);
   });
 
-  it('expect to move down one grid block when segment hits the left canvas wall', function() {
+  it('expects to move down one grid block when segment hits the left canvas wall', function() {
     let centipedeSeg = new CentipedeSeg(12, 36, -6, 24, 12, 'blue');
     
     centipedeSeg.move();
@@ -71,7 +71,7 @@ describe('CentipedeSeg', function() {
     expect(centipedeSeg.y).to.equal(60);
   });
 
-  it('expect to switch x direction when segment hits the right canvas wall', function() {
+  it('expects to switch x direction when segment hits the right canvas wall', function() {
     let centipedeSeg = new CentipedeSeg(708, 36, 6, 24, 12, 'blue');
 
     centipedeSeg.move();
@@ -83,7 +83,7 @@ describe('CentipedeSeg', function() {
     expect(centipedeSeg.dx).to.equal(-6);
   });
 
-  it('expect to move down one grid block when segment hits the right canvas wall', function() {
+  it('expects to move down one grid block when segment hits the right canvas wall', function() {
     let centipedeSeg = new CentipedeSeg(708, 36, 6, 24, 12, 'blue');
 
     centipedeSeg.move();
@@ -95,7 +95,7 @@ describe('CentipedeSeg', function() {
     expect(centipedeSeg.y).to.equal(60);    
   });
 
-  it('expect to change y velocity and move up one grid when segment reaches the bottom canvas restraint', function() {
+  it('expects to change y velocity and move up one grid when segment reaches the bottom canvas restraint', function() {
     let centipedeSeg = new CentipedeSeg(12, 730, -6, 24, 12, 'blue');
     centipedeSeg.move();
 
@@ -108,7 +108,7 @@ describe('CentipedeSeg', function() {
     expect(centipedeSeg.y).to.equal(706);
   });
 
-  it('expect to constrain segment movement to bottom of canvas when it reaches bottom canvas restraint', function() {
+  it('expects to constrain segment movement to bottom of canvas when it reaches bottom canvas restraint', function() {
     let centipedeSeg = new CentipedeSeg(12, 730, -6, 24, 12, 'blue');
 
     centipedeSeg.move();
@@ -120,7 +120,7 @@ describe('CentipedeSeg', function() {
     expect(centipedeSeg.reachedLowerHalf).to.equal(true);
   });
 
-  it('expect to not go below 550 on the y axis if it has already reached lower half', function() {
+  it('expects to not go below 550 on the y axis if it has already reached lower half', function() {
     let centipedeSeg = new CentipedeSeg(672, 552, 48, -48, 12, 'blue');
     centipedeSeg.reachedLowerHalf = true;
     
@@ -132,14 +132,14 @@ describe('CentipedeSeg', function() {
     expect(centipedeSeg.y).to.equal(648);
   });
 
-  it('expect to collide with objects when they are in the same x and y plane', function() {
+  it('expects to collide with objects when they are in the same x and y plane', function() {
     let bullet = new Bullet(120, 118);
     let collision = centipedeSeg.isColliding(bullet);
 
     expect(collision).to.equal(true);
   });
 
-  it('expect to detect collision with mushrooms and change the x velocity', function() {
+  it('expects to detect collision with mushrooms and change the x velocity', function() {
     let shroom = new Mushroom(130, 118);
     let shroomArray = [shroom];
 
@@ -148,7 +148,7 @@ describe('CentipedeSeg', function() {
     expect(centipedeSeg.dx).to.equal(-6);
   });
 
-  it('expect to detect collision with mushrooms and move down one grid', function() {
+  it('expects to detect collision with mushrooms and move down one grid', function() {
     let shroom = new Mushroom(130, 118);
     let shroomArray = [shroom];
 
@@ -157,9 +157,8 @@ describe('CentipedeSeg', function() {
     expect(centipedeSeg.y).to.equal(144);
   });
 
-  it('expect to detect collision with player and call player.die method to set player x, y and reduce lives', function() {
+  it('expects to detect collision with player and call player.die method to set player x, y and reduce lives', function() {
     let player = new Player(130, 118, 6, 6, 'blue');
-    let game = new Game();
 
     expect(player.lives).to.equal(3);
 
--- test/centipede-whole-test.js
+++ test/centipede-whole-test.js
@@ -3,6 +3,8 @@ const expect = chai.expect;
 
 const CentipedeWhole = require('../lib/centipede-whole.js');
 const CentipedeSeg = require('../lib/centipede-seg.js');
+const Bullet = require('../lib/bullet.js');
+const Mushroom = require('../lib/mushroom.js');
 
 describe('CentipedeWhole', function() {
   let centipede;
@@ -33,7 +35,26 @@ describe('CentipedeWhole', function() {
     expect(centipede.segmentsArray[0][1].color).to.equal('red');
   });
 
-  it('expect ', function() {
+  it('expect centipede to split into new arrays when there is a bullet collision', function() {
+    let shroomArray = [];
+    let bullet = new Bullet(144, 120);
+    let bulletArray = [bullet];
+    let seg1 = new CentipedeSeg(120, 120, 6, 6, 12);
+    let seg2 = new CentipedeSeg(144, 120, 6, 6, 12);
+    let seg3 = new CentipedeSeg(168, 120, 6, 6, 12);
+    centipede.segmentsArray[0] = [seg1, seg2, seg3];
+
+    expect(centipede.segmentsArray[0].length).to.equal(3);
+
+    centipede.centBulletCollision(bulletArray, shroomArray);
+
+    expect(shroomArray.length).to.equal(1);
+    expect(shroomArray[0].x).to.equal(144);
+    expect(shroomArray[0].y).to.equal(120);
+    expect(centipede.segmentsArray.length).to.equal(2);
+    expect(centipede.segmentsArray[0].length).to.equal(1);
+    expect(centipede.segmentsArray[1].length).to.equal(1);
+    expect(bulletArray.length).to.equal(0);
 
   });  
 })
